6a94bf5c185c7c66d6428a4fdf6dd7d6afb6460e
IsaiasDiaz20 <isaias.mercado@alumnos.udg.mx>
2026-02-22 21:49:55 -0600

    apiBase

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       // En Railway configuraremos NUXT_PUBLIC_API_BASE
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000/api'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://pruebafullstack-production.up.railway.app/api/'
     }
   },
 
